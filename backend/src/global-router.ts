237b9b0fd4b11d0096e11ecefda94ffdf962882e
AlanC8 <askvizi@bk.ru>
2024-07-27 17:13:38 +0500

    1.0.0: boilerplate

---
+++ backend/src/global-router.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import userRouter from './user/user-router';
+// other routers can be imported here
+
+const globalRouter = Router();
+
+globalRouter.use(userRouter);
+
+export default globalRouter;
